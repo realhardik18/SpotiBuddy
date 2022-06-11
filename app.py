79b1c27d2a68eebd535e3e22bc8dc93626f8d8cc
realhardik18 <realhardik18@gmail.com>
2022-06-11 22:57:42 +0530

    able to login and get data

--- app.py
+++ app.py
@@ -1,39 +1,71 @@
-from os import name
-from flask import Flask, request, url_for, session, redirect
-import creds
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask, url_for, session, request, redirect
+import time
+import creds
 
 app = Flask(__name__)
 
-app.secret_key = 'realhardik18isverycool'
-app.config['SESSION_COOKIE_NAME'] = 'Realhardik18LovesCookies'
+app.secret_key = 'realhardik18iscool'
+app.config['SESSION_COOKIE_NAME'] = 'realhardik18LovesCOOkies'
 
 
 @app.route('/')
 def login():
     sp_oauth = create_spotify_oauth()
     auth_url = sp_oauth.get_authorize_url()
+    print(auth_url)
     return redirect(auth_url)
 
 
-@app.route('/tracks')
-def tracks():
-    return 'tracks of user'
-
-
 @app.route('/redirect')
-def redirectToPage():
-    return "redirect"
+def authorize():
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session["token_info"] = token_info
+    return redirect("/data")
+
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
+
+
+@app.route('/data')
+def get_all_tracks():
+    session['token_info'], authorized = get_token()
+    session.modified = True
+    if not authorized:
+        return redirect('/')
+    return 'hello'
+
+
+def get_token():
+    token_valid = False
+    token_info = session.get("token_info", {})
+    if not (session.get('token_info', False)):
+        token_valid = False
+        return token_info, token_valid
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+    if is_token_expired:
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(
+            session.get('token_info').get('refresh_token'))
+    token_valid = True
+    return token_info, token_valid
 
 
 def create_spotify_oauth():
     return SpotifyOAuth(
         client_id=creds.client_id,
         client_secret=creds.client_secret,
-        redirect_uri=url_for('redirectToPage', _external=True),
-        scope='user-library-read'
-    )
+        redirect_uri=url_for('authorize', _external=True),
+        scope="user-library-read")
 
 
 app.run()
