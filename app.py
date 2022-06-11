7429de835a31e35b2dc6e26db0d165822fa5905b
realhardik18 <realhardik18@gmail.com>
2022-06-11 22:10:27 +0530

    implemented logging in with spotify

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from os import name
 from flask import Flask, request, url_for, session, redirect
+import creds
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -10,13 +11,29 @@ app.config['SESSION_COOKIE_NAME'] = 'Realhardik18LovesCookies'
 
 
 @app.route('/')
-def index():
-    return 'homepage'
+def login():
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
 
 
 @app.route('/tracks')
 def tracks():
     return 'tracks of user'
+
+
+@app.route('/redirect')
+def redirectToPage():
+    return "redirect"
+
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id=creds.client_id,
+        client_secret=creds.client_secret,
+        redirect_uri=url_for('redirectToPage', _external=True),
+        scope='user-library-read'
+    )
 
 
 app.run()
